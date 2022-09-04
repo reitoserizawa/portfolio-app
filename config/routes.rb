bbe5a829a42ce4684ff47f102e573847ced22271
ReitoSerizawa <s.reitiger@gmail.com>
2022-09-04 17:53:00 -0400

    route edited to only resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
   resources :contacts, only: [:index, :show, :create]
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
